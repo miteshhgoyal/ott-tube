030ae74008b7f8fa785028ad05fbda37d433ca3c
Mitesh Goyal <miteshgoyal.00@gmail.com>
2025-11-30 15:47:48 +0530

    debuggings

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -131,17 +131,21 @@ userSchema.pre('save', async function (next) {
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+// userSchema.pre('save', async function (next) {
+//     if (!this.isModified('password')) return next();
 
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+// });
 
 // Method to compare passwords
+// userSchema.methods.comparePassword = async function (candidatePassword) {
+//     return bcrypt.compare(candidatePassword, this.password);
+// };
+
 userSchema.methods.comparePassword = async function (candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
+    return candidatePassword === this.password;
 };
 
 export default mongoose.model('User', userSchema);
